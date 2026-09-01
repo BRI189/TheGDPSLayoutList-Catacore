46f9d79c7b914b5db38eb59931c19f18d678c8ff
BRI189 <brianfonseca189@gmail.com>
2026-09-01 18:28:18 -0500

    list and points changes

--- js/score.js
+++ js/score.js
@@ -11,20 +11,17 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
+    // Si el nivel está en Legacy List (más del 60), da 0 puntos
+    if (rank > 60) {
         return 0;
     }
-    if (rank > 75 && percent < 100) {
+    // Si el nivel está en Extended List (del 31 al 60), exige el 100%
+    if (rank > 30 && percent < 100) {
         return 0;
     }
 
-    // Old formula
-    /*
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-24.9975 * Math.pow(rank - 1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
